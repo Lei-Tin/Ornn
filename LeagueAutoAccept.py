96411971f690f4461221a2d18f6dba97df3e98db
HLeiTR <sy.hung@mail.utoronto.ca>
2024-12-05 23:11:57 -0500

    [+] Initial commit, added basic functionalities

---
+++ LeagueAutoAccept.py
@@ -0,0 +1,24 @@
+from lcu_driver import Connector
+import asyncio
+
+connector = Connector()
+
+@connector.ready
+async def connect(connection):
+    print('LCU API attached.')
+
+
+@connector.ws.register('/lol-matchmaking/v1/search', event_types=('CREATE',))
+async def icon_changed(connection, event):
+    print('Matchmaking start event received')
+
+@connector.ws.register('/lol-matchmaking/v1/ready-check', event_types=('CREATE',))
+async def icon_changed(connection, event):
+    print('Matchmaking ready check event received')
+    await asyncio.sleep(1)
+
+    await connection.request('post', '/lol-matchmaking/v1/ready-check/accept')
+    print('Accept matchmaking')
+
+if __name__ == '__main__':
+    connector.start()
