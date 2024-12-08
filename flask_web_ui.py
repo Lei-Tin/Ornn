2e0eaf68545fea4603dbc97de03651009c0bc0fe
HLeiTR <sy.hung@mail.utoronto.ca>
2024-12-07 21:15:07 -0500

    [+] Included UI updates

--- flask_web_ui.py
+++ flask_web_ui.py
@@ -18,20 +18,40 @@ app = Flask(__name__)
 # Global settings dictionary
 settings = {
     "auto_accept_enabled": True,
+    "auto_lock_in_enabled": False,
+    "auto_ban_enabled": False,
 }
 
+# Auto lock-in list of champions
+auto_lock_in_champions = []
+
+# Auto ban list of champions
+auto_ban_list_champions = []
+
 @app.route('/')
-def settings():
+def serve_settings():
     # Serve the settings page HTML
     return render_template('settings.html', settings=settings)
 
 @app.route('/update', methods=['POST'])
 def update_settings():
     global settings
+    global auto_lock_in_champions, auto_ban_list_champions
+
     data = request.get_json()
+
     setting = data['setting']
     value = data['value']
-    settings[setting] = value
+
+    if setting == 'auto_lock_in_champions':
+        # Setting lock-in champions list
+        auto_lock_in_champions = value
+    elif setting == 'auto_ban_list_champions':
+        # Setting ban champions list
+        auto_ban_list_champions = value
+    else:
+        # Setting global settings
+        settings[setting] = value
 
     # Log the new updated settings
     logger.info(f"{setting} updated to {value}")
